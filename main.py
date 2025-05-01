b29dc883ebca56f34b1409231ab02b1dcec85fff
ashrafuldalif <140844323+ashrafuldalif@users.noreply.github.com>
2025-05-01 23:47:16 +0600

    main page

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+tree_names = ["Tree Name", "Garden Croton", "Bougainvillea"]
+tree_details = [
+    "Colorful ornamental shrub with variegated foliage in vibrant green, yellow, red, and orange hues.",
+    "Colorful ornamental shrub with variegated foliage in vibrant green, yellow, red, and orange hues.",
+    "Vibrant flowering vine featuring papery bracts in magenta, purple, and orange hues."
+]
+tree_images = [
+    "../static/images/VisualMatches.jpg",
+    "../static/images/GardenCroton.jpg",
+    "../static/images/Bougainvillea.jpg"
+]
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",tName=tree_names,tDetail=tree_details,tImage=tree_images)
+
+
+@app.route("/Tinfo.html")
+def tinfo():
+    return render_template("Tinfo.html")
+
+
+
+
+
+
+
+
 name = ["Alif lombu", "Farzana", "Alif ashraf", "Mehedi", "Rafi", "Sokina"]
 batch = ["27th", "27th", "26th", "26th", "24th", "20000th"]
 age = [20, 23, 22, 22, 25, 20]
 
 
+ 
 
 @app.route("/info")
 def info():
